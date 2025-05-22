02fa5b7e357c7178eeb0f99b17618e2529ea3180
wmdalboni <dalboni.willian@gmail.com>
2025-05-22 16:49:42 -0300

    20240422_1659: First Deploy

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from simulator import run_simulation
 from visualizer import plot_simulation, plot_box_segmentado_unico
 
 
-st.set_page_config(page_title="Simulação Monte Carlo de Apostas", layout="wide")
+st.set_page_config(page_title="Simulabets!", layout="wide")
 
-st.title("🎲 Simulação Monte Carlo de Apostas")
+st.title("🎲 Simule antes de Jogar nas Bets!")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    aposta = st.number_input("Valor da Aposta", min_value=0.0, value=10.0, step=1.0)
-    retorno = st.number_input("Taxa de Retorno (multiplicador)", min_value=0.0, value=2.0, step=0.1)
-    chance_perda = st.number_input("Chance de Perder (%)", 0, 100, 50)
+    aposta = st.number_input("Valor da Aposta", min_value=0.0, value=10.0, step=1.0, help="⚠️ Quanto dinheiro você vai apostar. Se não souber, deixe como está como exemplo.")
+    retorno = st.number_input("Multiplicador (Taxa de Retorno)", min_value=0.0, value=2.0, step=0.1,help="⚠️ Quantas vezes você multiplicará sua aposta se você ganhar. Ex: Um valor 2 significa dobrar a aposta ao vencer. Se não souber, coloque uma média aproximada e arredondado para baixo.")
+    chance_perda = st.number_input("Chance de Perder (%)", 0, 100, 95, help="⚠️ Qual sua chance de perder a cada aposta em \%. Ex: 95 siginifica 95\% de perder. Se não souber, prefira 95.")
 
 with col2:
-    turnos = st.number_input("Quantidade de Turnos (Eixo X)", 1, 1000, 50)
-    apostadores = st.number_input("Número de Apostadores (iterações)", min_value=1, value=1000, step=1)
+    turnos = st.number_input("Quantidade de Apostas", 1, 1000, 50, step=1, help="⚠️ O valor não pode ser negativo.")
+    apostadores = st.number_input("Número de Apostadores", min_value=1, value=10000, step=1, max_value=100000, help="Ele simulará não só sua jogada mas vários outros como você. Mais pessoas jogando significa mais precisão. Se não souber, deixe como está.")
 
     col3, col4, col5, col6 = st.columns(4)
 
@@ -53,10 +53,24 @@ with col2:
             unsafe_allow_html=True
         )
 
+st.write("")
+if valor_esperado < 0:
+    mensagem = f"Com {turnos} apostas, você perderá R$ {abs(media_esperada):.2f} ao final!"
+else:
+    mensagem = f"Confira os dados inseridos porque, com o devido tempo, a Bet nunca perde!"
+
+st.markdown(
+    f"**ATENÇÃO**<br><span style='color:{cor}'> {mensagem} </span>",
+    unsafe_allow_html=True
+)
+
+
 if st.button("Iniciar Simulação"):
     resultados = run_simulation(aposta, retorno, turnos, chance_perda, apostadores)
-
-    st.dataframe(resultados)
+    with st.expander("**Não acredita nos dados?**", expanded=False):
+        st.markdown("Em dúvida se os gráficos estão certos? \n Confira os dados da simulação. Cada linha representa um apostador e cada coluna representa um turno.")
+        st.markdown("O saldo final de cada apostador é a última coluna.")
+        st.dataframe(resultados)
 
     st.pyplot(plot_simulation(resultados))
 
