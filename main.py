d934a4c4b556c352921a5829ce15da9a44eabf12
wmdalboni <dalboni.willian@gmail.com>
2025-05-22 20:29:50 -0300

    20250522_2029: Correcao no titulo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pix_key = st.secrets.get("PIX_KEY", None)
 st.set_page_config(page_title="Simulabets!", layout="wide")
 
 # Defino o título principal que vai aparecer no topo da aplicação.
-st.title("🎲🎲🎲 SimulaBet - Simule suas apostas! 🎲🎲🎲")
+st.title("🎲 SimulaBet - Simule suas apostas! 🎲")
 
 # Crio duas colunas para organizar os inputs do usuário lado a lado, assim fica mais limpo e intuitivo.
 col1, col2 = st.columns(2)
